f7454da608ced935b6f7e4aa6db1498d7b6d1ba7
Arnau Bertrans Mas <arnaubertrans@gmail.com>
2025-04-27 22:31:34 +0200

    cookies integration basic pt 1.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,17 +3,32 @@ import Header from '../components/ui/Header';
 import Sidebar from '../components/ui/Sidebar';
 import DailyInfo from '../components/DailyInfo';
 import { getUser } from '../services/authService';
+import Cookies from '../components/ui/Cookies';
 
 const Home = () => {
   const [userStreak, setUserStreak] = useState(0);
+  const [showCookies, setShowCookies] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const user = await getUser();
-      // get user-streak and upload it to localStorage
-      const streak = user.streak.length || 0;
-      setUserStreak(streak);
-      localStorage.setItem('user_streak', streak.toString());
+      try {
+        // get logged user
+        const user = await getUser();
+        if (user) {
+          const streak = user.streak.length || 0;
+          setUserStreak(streak);
+          localStorage.setItem('user_streak', streak.toString());
+
+          // check if cookies modal was already shown
+          // and if has been accpeted
+          const cookiesShown = localStorage.getItem('cookies_consent_shown');
+          if (cookiesShown !== 'true') {
+            setShowCookies(true);
+          }
+        }
+      } catch (error) {
+        console.error('Failed to fetch user:', error);
+      }
     };
 
     // check if localStorage has streak already
@@ -25,11 +40,17 @@ const Home = () => {
     fetchUser();
   }, []);
 
+  const handleCookieAction = () => {
+    // nofify child comp
+    setShowCookies(false);
+  };
+
   return (
     <>
       <Header />
       <Sidebar />
       <DailyInfo streak={userStreak} />
+      {showCookies && <Cookies onAction={handleCookieAction} />}
     </>
   );
 };
