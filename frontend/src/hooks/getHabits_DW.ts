07e980331b149fba9b6ae2f09d6611b99611a48a
Arnau Bertrans Mas <arnaubertrans@gmail.com>
2025-04-28 21:47:02 +0200

    Habits retrieved correctly

--- frontend/src/hooks/getHabits_DW.ts
+++ frontend/src/hooks/getHabits_DW.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 import HabitInterface from '../types/habit';
 import { getHabits } from '../services/habitService';
 
@@ -11,44 +11,50 @@ const useHabits = ({ dayOfWeek }: UseHabitsProps) => {
   const [habitsWeekly, setHabitsWeekly] = useState<HabitInterface[]>([]);
   const [loading, setLoading] = useState(false);
 
-  // Get the week days remaining from today
-  const weekDays = useMemo(() => {
-    return Array.from({ length: 7 }, (_, i) => (dayOfWeek + i) % 7);
-  }, [dayOfWeek]);
-
-  const getHabitsDaily = useCallback(async () => {
+  const fetchHabits = useCallback(async () => {
     setLoading(true);
     try {
       const habits = await getHabits();
-      // filter habits for today's day
-      const filteredHabitsToday = habits.filter((habit: HabitInterface) =>
-        habit.days.includes(dayOfWeek),
-      );
-      // set it in the state
+
+      // filter habits for todays day
+      const filteredHabitsToday = habits.filter((habit: HabitInterface) => {
+        // if freq == daily apply it auto to days arr
+        if (habit.frequency === 'daily') {
+          return true;
+        }
+
+        // other frequencies, check days arr
+        const daysArray = Array.isArray(habit.days)
+          ? habit.days
+          : String(habit.days)
+              .split(',')
+              .map((d) => parseInt(d.trim())); // convert into int ignoring spaces
+
+        const isForToday = daysArray.includes(dayOfWeek);
+        return isForToday;
+      });
+
+      // craete new arr appending habits at the end
+      const filteredHabitsWeekly = [...habits];
+
+      // update states
       setHabitsToday(filteredHabitsToday);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  }, [dayOfWeek]);
-
-  const getHabitsWeekly = useCallback(async () => {
-    setLoading(true);
-    try {
-      const habits = await getHabits();
-      // filter habits for the week days
-      // iterate to habit w filter, check if some days math the day of weekDays if there's match, include it to arr
-      const filteredHabitsWeekly = habits.filter((habit: HabitInterface) =>
-        habit.days.some((day) => weekDays.includes(day)),
-      );
       setHabitsWeekly(filteredHabitsWeekly);
     } catch (err) {
       console.error(err);
     } finally {
       setLoading(false);
     }
-  }, [weekDays]);
+  }, [dayOfWeek]);
+
+  // memorize functions w useCallback
+  const getHabitsDaily = useCallback(() => {
+    return fetchHabits();
+  }, [fetchHabits]);
+
+  const getHabitsWeekly = useCallback(() => {
+    return fetchHabits();
+  }, [fetchHabits]);
 
   return {
     habitsToday,
